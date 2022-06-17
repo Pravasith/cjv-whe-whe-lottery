d78065b4ecdf0f9fc575bba75e02389629c23594
pravasith <pravasith@gmail.com>
2022-06-17 14:47:50 -0400

    First component

---
+++ src/Components/Title.jsx
@@ -0,0 +1,11 @@
+import "../CSSFiles/Title.css"
+
+const Title = () => {
+    return (
+        <header className="header-title">
+            <h1>WHE WHE on D'Avenue</h1>
+        </header>
+    )
+}
+
+export default Title
